6d5e4303d11c6738d0606091a1553caa76a7af72
Samsovansoukea <61857727+Samsovansoukea@users.noreply.github.com>
2020-03-06 10:47:08 +0700

    Add files via upload

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    string name;
-    cout<<"Enter the name:";
-    cin>>name;
-    cout<<"Hello "<<name<<"\t";
+    int n1=5,n2=10;
+    int *p1,*p2;
+    p1=&n1;
+    cout<<*p1<<"\t"<<n1<<endl;
+    p2=&n2;
+    cout<<*p2<<"\t"<<n2<<endl;
+    p2=p1;
+    cout<<*p2<<"\t"<<*p2<<endl;
+    return 0;
 }
